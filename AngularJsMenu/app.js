66ccd4477d0fee7235b53bc8230bd916c2ffbe27
Burak Aykanat <burak.aykanat12@gmail.com>
2017-09-15 21:23:43 +0300

    Add files via upload

---
+++ AngularJsMenu/app.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module("AngularJs-Menu", []);
+
+app.controller("Controller", function ($scope) {
+
+    $scope.aValue = function (value) {
+        $scope.tab = value;
+    }
+
+    $scope.tabValue = function (value) {
+        return $scope.tab == value;
+    }
+
+    $scope.items = ["Content 1", "Content 2", "Content 3", "Content 4"];
+
+});
